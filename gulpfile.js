28f1a43b6ce709b40b8ba0995d3d1976c3dcd67a
Adrian Miller <adrianjonmiller@gmail.com>
2017-04-25 08:30:36 -0700

    added style guide

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,29 @@ const gulp = require('gulp');
 const path = require('path');
 const sass = require('gulp-sass');
 const sassLint = require('gulp-sass-lint');
+const strip = require('gulp-strip-comments');
+const styleguide = require('gulp-styleguide');
+const browserSync = require('browser-sync').create();
 
 const dist = path.join(__dirname, 'dist/**/*.s+(a|c)ss')
 
+const options = {
+    site: {
+        title: 'Test Pattern Library'
+    },
+    src: {
+      css: [dist, '!./dist/bower_components/*']
+    },
+    dest: {
+      html: 'styleguide'
+    }
+};
+
+
+gulp.task('templates', styleguide.templates(options));
+gulp.task('build', ['templates'], styleguide.build(options));
+
 gulp.task('lint', function () {
-  console.log(dist)
   return gulp.src(dist)
     .pipe(sassLint({
       configFile: '.sass-lint.yml',
@@ -20,6 +38,22 @@ gulp.task('lint', function () {
     .pipe(sassLint.failOnError())
   })
 
-gulp.task('default', ['lint'], function () {
-  gulp.watch(dist, {interval:2000}, ['lint']);
-});
+gulp.task('sass', ['lint'], function () {
+  return gulp.src(dist)
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+  });
+
+gulp.task('serve', function () {
+  browserSync.init({
+        server: {
+          baseDir: "./styleguide"
+        }
+    });
+
+    gulp.watch(dist, {interval:2000}, ['sass', 'build']);
+    gulp.watch("./styleguide/*.html").on('change', browserSync.reload);
+  })
+
+gulp.task('default', ['serve']);
